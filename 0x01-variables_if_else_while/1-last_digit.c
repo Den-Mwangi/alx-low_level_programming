0659cca6beb41fc5a617af9bdb75424b06f81f1a
Den-Mwangi <deniskamunya8@gmail.com>
2022-07-01 09:06:13 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,49 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
+#include <time.h>
+
+#include <stdlib.h>
+
+
+
 /**
- *main - prints the last digit of the random
- *number stored in the variable n
- *Return: Always 0 (Success)
- */
+ *
+ *  * main - Determine the last digit of a random number and compare it to 5
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
-int n, 1;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-if (l > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n", n, l);								
-}
-else if (l == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, l)	
-}
-else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);							
-}
+
+		int n;
+
+			int last;
+
+
+
+				srand(time(0));
+
+					n = rand() - RAND_MAX / 2;
+
+						last = n % 10;
+
+
+
+							if (last > 5)
+
+										printf("Last digit of %i is %i and is greater than 5\n", n, last);
+
+								else if (last == 0)
+
+											printf("Last digit of %i is %i and is 0\n", n, last);
+
+									else if (last < 6)
+
+											printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Write an alphabet in lowercase followed by a new line
+ *Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);						
+c++;
+}
+putchar('\n');
+return (0);
+}

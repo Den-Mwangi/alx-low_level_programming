8c9eef5cb44c14af534c8ea0e9ed7a32120067df
Den-Mwangi <deniskamunya8@gmail.com>
2022-07-01 07:45:58 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,22 +4,21 @@
 
 /**
  *main - prints the last digit of the random
- *
- * number stored in the variable n
- *
+ *number stored in the variable n
  *Return: Always 0 (Success)
  */
 int main(void)
+
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is %d ", n, n % 10);
-if (n % 10 > 5)							
+if (n % 10 > 5)
 printf("and is greater than 5\n");
-else if (n % 10 == 0)						
+else if (n % 10 == 0)
 printf("and is 0\n");
 else if (n % 10 < 6 && n % 10 != 0)
-printf("and is less than 6 and not 0\n");
+printf("and is less than 6 and not 0\n");			
 return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,3 @@
 This an assignment ALX low level programmiing language.
+0-positive or negative.c Prints is positive, is zero or is negative when a randomly generated number is any of these cases
+1-last digit.c Prints the last digit of a randomly generated number

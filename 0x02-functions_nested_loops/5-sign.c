f4e99037a37037845d3b1c1a08d695c96ad89347
Den-Mwangi <deniskamunya8@gmail.com>
2022-07-05 08:59:42 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: the number tobe checked
+ *
+ * Return: 1 and print + if n is greater than zero
+ * 0 and print zero if n is zero
+ * -1 and print - if n is less than zero
+ */
+int print_sign(int n)
+{
+int sign = 0;
+
+if (n > 0)
+{
+sign = 1;
+_putchar('+');
+}
+else if (n < 0)
+{
+sign = -1;
+-putchar('-');
+}
+else
+{
+sign = 0;
+_putchar('0');
+}
+return (sign);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@ function that prints the alphabet, in lowercase, followed by a new line.
 function that prints 10 times the alphabet, in lowercase, followed by a new line.
 function that checks for lowercase character.
 function that checks for alphabetic character.
+function that prints the sign of a number.

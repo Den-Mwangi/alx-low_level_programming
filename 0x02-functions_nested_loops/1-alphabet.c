6ba90ef6ca3639c55016316592a5c76a561564b0
Den-Mwangi <deniskamunya8@gmail.com>
2022-07-05 07:49:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * prints the alphabet, in lowercase
+ *
+ * Return 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+		_putchar(c++);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 program that prints _putchar, followed by a new line.
+function that prints the alphabet, in lowercase, followed by a new line.

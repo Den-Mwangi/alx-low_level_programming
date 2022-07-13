df0ea47e899118bb0c55105b80e432c48271fb49
Den-Mwangi <deniskamunya8@gmail.com>
2022-07-13 15:27:46 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
 
-int main(void)
-{
-	int n;
-	int a[5];
-	int *p;
+/**
+ * infinite_add - add 2 integers.
+ * @n1: integer
+ * @n2: integer
+ * @r: buffer
+ * size_r: size of r
+ * Return: char
+ */
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 *   - you are not allowed to code anything else than this line of code
-	 */
-	p[5] = 98;
-	 /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+int _atoi(char *s)
+{
+	int sign = 1, resp = 0, firstNum;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+void int_to_string(int n)
+{
+	int divisor = 1, i, resp;
+
+	for (i = 0; n / divisor > 9; i++)
+	{
+		divisor *= 10;
+	}
+
+	char str[i];
+
+	for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
+	{
+		resp = n / divisor;
+		str[cmpt] = '0' + resp;
+		n = n - resp * divisor;
+	}
+	str[i] = ('0' + n);
+
+}
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int sum, a, b;
+	a = _atoi(n1);
+	b = _atoi(n2);
+
+	sum = a + b;
 }

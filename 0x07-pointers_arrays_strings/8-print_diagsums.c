86fefb5ab5b9af9eb9c7f61981a9d2df07952574
Den-Mwangi <deniskamunya8@gmail.com>
2022-07-18 08:29:57 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,18 +3,17 @@
 
 /**
  * print_diagsums - description
- * @a: 2d array of int types
- * @size: size of array (square)
+ * @a: a pointer
+ * @size: size
  */
-
 void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i];
-		sum2 += a[size - i - 1];
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
 	}
 	printf("%d, ", sum1);
 	printf("%d\n", sum2);

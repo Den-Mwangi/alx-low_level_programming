6940d3221871f201b3f0baf484cc3b44313ac5f4
Den-Mwangi <deniskamunya8@gmail.com>
2022-07-20 15:28:37 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,9 +12,8 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s2 != '\0' && *s2 == '*')
 		{
-		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
 		}
-		return (*s2 == '\0');
 	}
 
 	if (*s2 == '*')
